a0eb78c1fbd71b9bb612c258f07a647d4c0febe5
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-06-25 17:37:03 -0300

    :sparkles: Classe book criada

--- src/main/java/com/trybe/spring/application/BookController.java
+++ src/main/java/com/trybe/spring/application/BookController.java
@@ -1,5 +1,12 @@
 package com.trybe.spring.application;
 
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Path("/api/books")
+
 public class BookController {
 
 
--- src/main/java/com/trybe/spring/domain/Book.java
+++ src/main/java/com/trybe/spring/domain/Book.java
@@ -1,6 +1,54 @@
 package com.trybe.spring.domain;
 
-public class Book {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+public class Book {
+  private List<Book> books = new ArrayList<>();
+
+  private UUID id;
+
+  private String name;
+
+  private String author;
+
+  public Book(String name, String author) {
+      this.id = UUID.randomUUID();
+      this.name = name;
+      this.author = author;
+  }
+
+  public List<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(List<Book> books) {
+    this.books = books;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public void setAuthor(String author) {
+    this.author = author;
+  }
 }
 
